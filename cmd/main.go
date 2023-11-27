d1e128385ac44f27dc7774c3975934e003f9e5f6
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-11-27 13:31:45 +0700

    🚀 Add main.go

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+	_ "github.com/muhamadrafipamungkas/cloud-function-golang/src/hello"
+	_ "github.com/muhamadrafipamungkas/cloud-function-golang/src/user"
+)
+
+func main() {
+	// Use PORT environment variable, or default to 8080.
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
