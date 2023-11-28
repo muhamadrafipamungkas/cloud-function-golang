2428dbc8878056b184e26f1cfc644ff8d259805e
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-11-28 21:16:03 +0700

    Make entrypoint

--- entrypoint.go
+++ entrypoint.go
@@ -1,14 +1,29 @@
-package cloudfunctiongolang
+package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	Hello "github.com/muhamadrafipamungkas/cloud-function-golang/src/hello"
-	User "github.com/muhamadrafipamungkas/cloud-function-golang/src/user"
+	"github.com/muhamadrafipamungkas/cloud-function-golang/src/hello"
+	"github.com/muhamadrafipamungkas/cloud-function-golang/src/user"
 )
 
 func init() {
-	functions.HTTP("HelloWorld", Hello.HelloWorld)
-	functions.HTTP("HelloDynamic", Hello.HelloDynamic)
-	functions.HTTP("HelloUser", User.HelloUser)
+	functions.HTTP("HelloWorld", hello.HelloWorld)
+	functions.HTTP("HelloDynamic", hello.HelloDynamic)
+	functions.HTTP("HelloUser", user.HelloUser)
+}
+
+func main() {
+	// Use PORT environment variable, or default to 8080.
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
 }
